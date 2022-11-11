f386822fd8d250d1de84d84dc71a4f8a92ceb999
REACT-DEVELOPER-IBROKHIM <ibrohim123451@gmail.com>
2022-11-11 14:48:29 +0500

    Boilerplate

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import Home from "./routes/home/Home";
+import Login from "./routes/login/Login";
+import SignUp from "./routes/signup/SignUp";
+import { Route } from 'react-router-dom';
+
 // https://pngimg.com/uploads/amazon/amazon_PNG25.png
 function App() {
   return (
     <div>
-      
+      <Route exact path="/">
+        <Home/>
+      </Route>
+      <Route path="/login">
+          <Login/>
+      </Route>
+      <Route path="/signup">
+          <SignUp/>
+      </Route>
     </div>
   );
 }
---
+++ src/routes/customer-service/CustomerServices.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomerServices = () => {
+  return (
+    <div>CustomerServices</div>
+  )
+}
+
+export default CustomerServices
