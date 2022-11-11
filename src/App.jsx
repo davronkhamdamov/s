7af889ca295ad74377e2f526a7d529f4504b3300
REACT-DEVELOPER-IBROKHIM <ibrohim123451@gmail.com>
2022-11-11 15:43:13 +0500

    Restore

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,10 @@
-import Home from "./routes/home/Home";
-import Login from "./routes/login/Login";
-import SignUp from "./routes/signup/SignUp";
-import CustomerService from './routes/customer-service/CustomerServices'
-import { Route } from 'react-router-dom';
+import Routes from "./routes/Routes";
 
 function App() {
   return (
-    <div>
-      <Route exact path="/">
-        <Home/>
-      </Route>
-      <Route path="/login">
-          <Login/>
-      </Route>
-      <Route path="/signup">
-          <SignUp/>
-      </Route>
-      <Route path="/customer-service">
-        <CustomerService />
-      </Route>
-    </div>
+    <>
+      <Routes/>
+    </>
   );
 }
 
